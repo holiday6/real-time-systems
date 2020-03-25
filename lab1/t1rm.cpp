3cc329b92794b90c712e0ab754cd0919c8433ab4
holiday6 <d0392517@mail.fcu.edu.tw>
2020-03-25 16:39:27 +0800

    complete bash test.sh test
    add file:
    1ms rm t1rm t2rm t3rm

---
+++ lab1/t1rm.cpp
@@ -0,0 +1,90 @@
+#define _GNU_SOURCE 1
+#include <sched.h>
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include <cmath>
+#include <unistd.h>
+#include <signal.h>
+
+sig_atomic_t t = 0;
+int m = 0;
+void collectData (int param)
+{
+	//std::cout << ::m <<" "<< ::t << std::endl;
+	std::cout << (double)::m/::t << std::endl;
+
+}
+
+void pinCPU (int cpu_number)
+{
+	cpu_set_t mask;
+	CPU_ZERO(&mask);
+
+	CPU_SET(cpu_number, &mask);
+
+	if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1)
+	{
+		perror("sched_setaffinity");
+		exit(EXIT_FAILURE);
+	}
+}
+void setSchedulingPolicy (int policy, int priority)
+{
+	sched_param sched;
+	sched_getparam(0, &sched);
+	sched.sched_priority = priority;
+	if (sched_setscheduler(0, policy, &sched)) {
+		perror("sched_setscheduler");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void workload_1ms (void)
+{
+	int repeat = 130000; // tune this for the right amount of workload
+	for (int i = 0; i <= repeat; i++)
+	{
+		sqrt(i);
+		// add some computation here (e.g., use sqrt() in cmath)
+	}
+}
+
+int main (void)
+{
+
+	void (*prev_handler)(int);
+	prev_handler = signal (SIGINT, collectData);
+
+	pinCPU(0);
+	int period = 5000; // unit: microsecond
+	int delta;
+	setSchedulingPolicy (SCHED_FIFO, 99);
+	while (1)
+	{
+		std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+		for (int j = 0; j < 2; j++)
+		{
+			workload_1ms ();
+		}
+		std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+		delta = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+		//if(endTime > (startTime + std::chrono::microseconds(5000)))
+		//{
+		//	::m++;
+		//}	
+		::t++;
+		if (delta > period)
+		{
+			::m++;	
+			continue;
+		}
+		else
+		{
+
+			usleep (period-delta);
+		}
+	}
+	return 0;
+}
+
